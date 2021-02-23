2068dcd06047e8396c153493804c630c43103d97
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-02-23 11:05:56 +0100

    Create branch with four section | My First App in React using an API

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+import { getGifs } from '../helpers/getGifs'
+
+export const useFetchGifs = ( category ) => {
+
+    const [state, setState] = useState({
+        data: [],
+        loading: true
+    });
+
+    useEffect( () => {
+        getGifs( category )
+            .then( imgs => {
+                setState({
+                    data: imgs,
+                    loading: false
+                })
+            });
+    }, [category]);
+
+/*     setTimeout( () => {
+        setState({
+            data: [1,2,3,4,5],
+            loading: false
+        });
+    }, 3000) */
+
+    return state;
+
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GifExpertApp from './GiftExpertApp';
+
+import './index.css';
+
+ReactDOM.render(
+  <GifExpertApp />,
+  document.getElementById('root')
+);
